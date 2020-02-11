688af2606f079f0abb56131518889a82eefb9fe7
sihadan <def6488@gmail.com>
2020-02-11 17:07:08 +0900

    [WIP] change API to use cloudflare workers

--- assets/burgerRequest.js
+++ assets/burgerRequest.js
@@ -2,8 +2,9 @@ import fetch, { Headers } from 'node-fetch'
 const crypto = require('crypto')
 
 const headers = {
-    'Host': 'app.burgerking.co.kr:443',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36',
+  'Host': 'app.burgerking.co.kr:443',
+  'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36',
+  'Content-Type': 'application/x-www-form-urlencoded',
 }
 
 const wrapCORS = (url) => {
@@ -15,9 +16,9 @@ const wrapCORS2 = (url) => {
 }
 
 const formatParams = (params) => {
-  return  Object.keys(params)
-            .map(k => k + '=' + params[k])
-            .join('&')
+  return Object.keys(params)
+    .map(k => k + '=' + params[k])
+    .join('&')
 }
 
 export const generateUDID = () => {
@@ -46,28 +47,64 @@ export const getCouponListCORS = (udid) => {
   const url = wrapCORS(baseUrl + formatParams(params))
   const urlAlt = wrapCORS2(baseUrl + formatParams(params))
   return fetch(url, { headers: headers })
-          .then(res => res.json())
-          .then(json => json.resultlist)
-          .catch(() => {
-            return fetch(urlAlt, { headers: headers })
-              .then(res => res.json())
-              .then(json => json.resultlist)
-          })
+    .then(res => res.json())
+    .then(json => json.resultlist)
+    .catch(() => {
+      return fetch(urlAlt, { headers: headers })
+        .then(res => res.json())
+        .then(json => json.resultlist)
+    })
 }
 
+// export const getCouponList = (udid) => {
+//   const apiUrl = 'https://burgerqueen-api.azurewebsites.net/api/couponlist?'
+//   const params = {
+//     udid,
+//   }
+
+//   const url = apiUrl + formatParams(params)
+
+//   return fetch(url)
+//     .then(res => res.json())
+//     .catch(() => {
+//       return getCouponListCORS(udid)
+//     })
+// }
+
 export const getCouponList = (udid) => {
-  const apiUrl = 'https://burgerqueen-api.azurewebsites.net/api/couponlist?'
+  const baseUrl = 'https://app.burgerking.co.kr/bkr-omni/BKR4001.json'
+  const apiUrl = 'https://cors.ryanking13.workers.dev/?u=' + baseUrl + '&host=' + headers.Host
+
   const params = {
-    udid,
+    header: {
+      result: 'true',
+      trcode: 'BKR4001',
+      platform: '01',
+      is_cryption: 'false',
+    },
+    body: {
+      cdCouponObj: '3',
+      udid: udid,
+    }
   }
 
-  const url = apiUrl + formatParams(params)
+  const data = {
+    message: encodeURIComponent(JSON.stringify(params)),
+  }
 
-  return fetch(url)
-          .then(res => res.json())
-          .catch(() => {
-            return getCouponListCORS(udid)
-          })
+  const searchParams = Object.entries(data).map(([key, val]) => `${key}=${val}`).join('&')
+
+  console.log(searchParams)
+
+  return fetch(apiUrl, {
+    method: 'POST',
+    mode: 'cors',
+    headers: headers,
+    body: searchParams,
+  })
+    .then(res => res.json())
+    .then(json => json.body.couponList)
+    .catch(() => "ERROR")
 }
 
 export const getCouponCodeCORS = (udid, couponPk) => {
@@ -84,13 +121,13 @@ export const getCouponCodeCORS = (udid, couponPk) => {
   const urlAlt = wrapCORS2(baseUrl + formatParams(params))
 
   return fetch(url)
-          .then(res => res.json())
-          .then(json => json.resultMap.couponPinData)
-          .catch(() => {
-            return fetch(urlAlt, { headers: headers })
-              .then(res => res.json())
-              .then(json => json.resultMap.couponPinData)
-          })
+    .then(res => res.json())
+    .then(json => json.resultMap.couponPinData)
+    .catch(() => {
+      return fetch(urlAlt, { headers: headers })
+        .then(res => res.json())
+        .then(json => json.resultMap.couponPinData)
+    })
 }
 
 export const getCouponCode = (udid, couponPk) => {
@@ -103,10 +140,10 @@ export const getCouponCode = (udid, couponPk) => {
   const url = apiUrl + formatParams(params)
 
   return fetch(url)
-          .then(res => res.json())
-          .catch(() => {
-            return getCouponCodeCORS(udid, couponPk)
-          })
+    .then(res => res.json())
+    .catch(() => {
+      return getCouponCodeCORS(udid, couponPk)
+    })
 }
 
 export const getSurveyCode = (code) => {
@@ -118,13 +155,13 @@ export const getSurveyCode = (code) => {
   const url = apiUrl + formatParams(params)
 
   return fetch(url)
-          .then(res => {
-            if (!res.ok) return { 'failMessage': res.text() }
-            return res.json()
-          })
-          .catch(() => {
-            return { 'failMessage': 'Undefined Error' }
-          })
+    .then(res => {
+      if (!res.ok) return { 'failMessage': res.text() }
+      return res.json()
+    })
+    .catch(() => {
+      return { 'failMessage': 'Undefined Error' }
+    })
 }
 
 export const init = () => {
